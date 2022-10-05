94062948d878724b2a041b650ac12b95c54dc343
nicolasBernaux <bernaux.n@gmail.com>
2022-10-05 15:32:27 +0200

    Fix errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 const purgecss = require('@fullhuman/postcss-purgecss');
 const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 module.exports = (ctx) => {
   return {
     plugins: [
-      require('autoprefixer'),
+      autoprefixer,
       ...(ctx.options.env === 'production'
         ? [
             cssnano({
